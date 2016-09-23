e96fa4c1fa7501e4a750040eac1faa52d525652f
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2016-09-23 10:25:46 +0200

    Code checking as a font family class extension

--- core/src/main/scala/net/lshift/typesetr/xml/attributes/FontFamily.scala
+++ core/src/main/scala/net/lshift/typesetr/xml/attributes/FontFamily.scala
@@ -7,7 +7,22 @@ case class FontFamily(name: String) extends StyleAttribute
 object FontFamily {
   lazy val Arial = FontFamily("Arial")
 
+  implicit class FontFamilyOps(val x: FontFamily) extends AnyVal {
+
+    def isCodeFont: Boolean = x.name.toLowerCase.endsWith(" mono") ||
+      FontFamily.codeFonts.contains(x)
+
+  }
+
   implicit def strToFont(name: String): FontFamily =
     FontFamily(name)
 
+  private lazy val codeFonts =
+    List[FontFamily](
+      "Anonymous Pro", "Consolas", "Courier", "Courier New", "Envy Code R",
+      "Fixed", "fixed", "GNU Unifont", "Inconsolata", "Inconsolata-g", "Lucida Console",
+      "M+ 1m", "Menlo", "Monaco", "Monofur", "OCR-A", "OCR-B",
+      "Pragmata Pro", "Source Code Pro", "Terminal", "Terminus")
+
 }
+
